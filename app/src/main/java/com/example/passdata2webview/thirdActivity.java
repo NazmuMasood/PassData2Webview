39b7c190e56a8b14c53b2356c07b95248df01713
NazmuMasood <44304205+NazmuMasood@users.noreply.github.com>
2019-05-24 00:47:12 +0600

    progressBar added

--- app/src/main/java/com/example/passdata2webview/thirdActivity.java
+++ app/src/main/java/com/example/passdata2webview/thirdActivity.java
@@ -1,11 +1,16 @@
 package com.example.passdata2webview;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.graphics.Bitmap;
 
 public class thirdActivity extends AppCompatActivity {
     WebView webView;
@@ -19,7 +24,20 @@ public class thirdActivity extends AppCompatActivity {
         String searchText = i.getStringExtra("search");
         searchText = "https://google.com/#q="+searchText;
         webView = findViewById(R.id.webview);
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
         webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if(progress < 100 && progressBar.getVisibility() == ProgressBar.GONE){
+                    progressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+
+                progressBar.setProgress(progress);
+                if(progress == 100) {
+                    progressBar.setVisibility(ProgressBar.GONE);
+                }
+            }
+        });
         webView.loadUrl(searchText);
 
         WebSettings webSettings = webView.getSettings();
